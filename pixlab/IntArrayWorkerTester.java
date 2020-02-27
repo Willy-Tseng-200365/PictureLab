41be580c2586a1908a1ee9c7cdb9d2de5cceae18
Willy-Tseng-200365 <56176606+Willy-Tseng-200365@users.noreply.github.com>
2020-02-27 15:05:30 -0800

    commit

--- pixlab/IntArrayWorkerTester.java
+++ pixlab/IntArrayWorkerTester.java
@@ -19,9 +19,25 @@ public class IntArrayWorkerTester
     worker.fillPattern1();
     System.out.println("fills with 2's on diagonal, 3's to left, and 1's to right");
     worker.print();
+    
   }
   
   /** Method to test getCount*/
+  public int getCount(int[][] num){
+      int ans = 0;  
+      for(int i = 0; i< num.length-1; i++)
+      {
+          for(int j = 0; j <num[i].length; j++)
+          {
+           ans = num[i][j] + ans;   
+              
+              
+            
+            }
+        }
+      return ans;
+    }
+
   public static void testGetCount()
  {
     IntArrayWorker worker = new IntArrayWorker();
@@ -29,6 +45,7 @@ public class IntArrayWorkerTester
    worker.setMatrix(nums);
    worker.fillPattern1();
     int count = worker.getCount(1);
+    
     System.out.println("Count should be 6 and count is " + count);
   }
   
@@ -53,49 +70,49 @@ public class IntArrayWorkerTester
   }
   
   /** Method to test getLargest */
- public static void testGetLargest()
-  { // test when largest is last
-    IntArrayWorker worker = new IntArrayWorker();
-    int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
-    worker.setMatrix(nums2);
-    int largest = worker.getLargest();
-    System.out.println("Largest should be 6 and is " + largest); 
+ //public static void testGetLargest()
+  //{ // test when largest is last
+    //IntArrayWorker worker = new IntArrayWorker();
+    //int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
+    //worker.setMatrix(nums2);
+    //int largest = worker.getLargest();
+    //System.out.println("Largest should be 6 and is " + largest); 
     // test when largest is first
-    int[][] nums3 = {{6, 2, 3}, {4, 5, 1}};
-    worker.setMatrix(nums3);
-    largest = worker.getLargest();
-    System.out.println("Largest should be 6 and is " + largest); 
+    //int[][] nums3 = {{6, 2, 3}, {4, 5, 1}};
+    //worker.setMatrix(nums3);
+    //largest = worker.getLargest();
+    //System.out.println("Largest should be 6 and is " + largest); 
     // test when largest is in the middle
-    int[][] nums4 = {{1, 2, 3}, {6, 5, 1}};
-    worker.setMatrix(nums4);
-    largest = worker.getLargest();
-    System.out.println("Largest should be 6 and is " + largest);
+    //int[][] nums4 = {{1, 2, 3}, {6, 5, 1}};
+    //worker.setMatrix(nums4);
+    //largest = worker.getLargest();
+    //System.out.println("Largest should be 6 and is " + largest);
     // test when duplicate largest
-    int[][] nums5 = {{6, 2, 6}, {4, 5, 1}};
-    worker.setMatrix(nums5);
-    largest = worker.getLargest();
-    System.out.println("Largest should be 6 and is " + largest);
-  }
+    //int[][] nums5 = {{6, 2, 6}, {4, 5, 1}};
+    //worker.setMatrix(nums5);
+    //largest = worker.getLargest();
+    //System.out.println("Largest should be 6 and is " + largest);
+  //}
   
   /** Method to test getColTotal */
-  public static void testGetColTotal()
-  {
-    IntArrayWorker worker = new IntArrayWorker();
-    int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
-    worker.setMatrix(nums2);
-    int total = worker.getColTotal(0);
-    System.out.println("Total for column 0 should be 5 and is " + total);
-    total = worker.getColTotal(1);
-    System.out.println("Total for column 1 should be 7 and is " + total);
-    total = worker.getColTotal(2);
-    System.out.println("Total for column 2 should be 9 and is " + total);
-  }
+ // public static void testGetColTotal()
+  //{
+  //  IntArrayWorker worker = new IntArrayWorker();
+    //int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
+  //  worker.setMatrix(nums2);
+  //  int total = worker.getColTotal(0);
+   // System.out.println("Total for column 0 should be 5 and is " + total);
+   // total = worker.getColTotal(1);
+   // System.out.println("Total for column 1 should be 7 and is " + total);
+   // total = worker.getColTotal(2);
+   // System.out.println("Total for column 2 should be 9 and is " + total);
+  //}
   
   public static void main(String[] args)
   {
     testSetMatrix();
     testFillPattern1();
-    //testGetCount(); 
+    testGetCount(); 
     testGetTotal();
     testGetTotalNested();
     //testGetLargest();
